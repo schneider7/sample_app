494fcb5bdf02dee4af43d56aeaa34f0da984acc1
Mike Schneider <mschneider3254@gmail.com>
2019-05-30 11:34:44 -0500

    Small fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index]
-  before_action :correct_user,   only: [:edit, :update]
+   before_action :set_user,       only: [:show, :edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
+ 
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
-  end
+      end
 
   def new
     @user = User.new
@@ -27,11 +28,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash.now[:success] = "Profile successfully updated."
       redirect_to @user
@@ -54,7 +53,10 @@ class UsersController < ApplicationController
       end 
 
       def correct_user
+        redirect_to root_url unless @user == current_user
+      end
+
+      def set_user
         @user = User.find(params[:id])
-        redirect_to(root_url) unless @user == current_user
       end
 end
