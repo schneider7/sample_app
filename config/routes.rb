59ca0b60fe2595d2619c7c757f34dd82047dd0a7
Mike Schneider <mschneider3254@gmail.com>
2019-06-14 11:18:38 -0500

    I think I fixed the SSL error now

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'  
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   'login',    to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/help',      to: 'static_pages#help'
+  get    '/about',     to: 'static_pages#about'
+  get    '/contact',   to: 'static_pages#contact'
+  get    '/signup',    to: 'users#new'
+  get    '/login',     to: 'sessions#new'
+  post   'login',      to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
   resources :users
 
   mount GitHooks::Engine, at: "/git_hooks"
